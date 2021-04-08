6d09a89ac75d5598d1f91166b66526043cd1e384
skarolk <skarolkiewicz@gmail.com>
2021-04-07 22:17:10 -0500

    setup marketing app config

---
+++ marketing/config/webpack.common.js
@@ -0,0 +1,19 @@
+module.exports = {
+  module: {
+    // loaders - process files
+    rules: [
+      {
+        // process any file that ends in mjs or js
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
