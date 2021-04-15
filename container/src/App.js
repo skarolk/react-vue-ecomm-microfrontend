e30903b16d3ebad6419e0b20438e29c2c3c35922
skarolk <skarolkiewicz@gmail.com>
2021-04-14 22:31:21 -0500

    updated prod yml for marketing mfe

--- container/src/App.js
+++ container/src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("automate cache clearing v2");
-
   return (
     <>
       <h1>Container App</h1>
