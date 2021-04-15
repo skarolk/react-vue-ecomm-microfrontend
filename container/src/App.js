8383a219da5f4e21516a1bbb6af70b7027fc1ffa
skarolk <skarolkiewicz@gmail.com>
2021-04-14 21:58:16 -0500

    fixing automation

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 
 export default () => {
-  console.log("automate cache clearing");
+  console.log("automate cache clearing v2");
 
   return (
     <>
