befe144b25359caefb70c80ff93891ddc60c5221
skarolk <skarolkiewicz@gmail.com>
2021-04-16 23:05:52 -0500

    added vue dashboard to container

---
+++ dashboard/config/webpack.dev.js
@@ -0,0 +1,45 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  output: {
+    publicPath: "http://localhost:8083/",
+  },
+  devServer: {
+    port: 8083,
+    historyApiFallback: {
+      index: "/index.html",
+    },
+    headers: {
+      // allows us to load up fonts without CORS errors
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      // used to declare global variable for app code in container
+      name: "dashboard",
+      // whatever filename you want to use here is fine
+      filename: "remoteEntry.js",
+      exposes: {
+        // container will as for Marketing file which will provide bootstrap contents
+        "./DashboardApp": "./src/bootstrap",
+      },
+      // array useful when you want to be specific about what packages you want to have shared
+      // shared: ["react", "react-dom"]
+      // you can share package.json dependencies as object
+      // DO NOT share dev dependencies
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+// second argument takes precedence over first argument
+module.exports = merge(commonConfig, devConfig);
---
+++ dashboard/src/bootstrap.js
@@ -0,0 +1,20 @@
+console.log("dashboard app running ...");
+import { createApp } from "vue";
+import Dashboard from "./components/Dashboard";
+
+// mount function to start up app
+const mount = (el) => {
+  const app = createApp(Dashboard);
+  app.mount(el);
+};
+
+// call mount immediately in development and isolation
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.getElementById("_dashboard-dev-root");
+
+  // empty object fixes error when not passing in onNavigate
+  devRoot && mount(devRoot);
+}
+
+// export mount function
+export { mount };
